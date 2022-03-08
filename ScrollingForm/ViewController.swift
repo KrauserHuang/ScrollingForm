87f2c27fb4ee496299f40b8573ab13887eb371ca
krauserhuang <crescentdence@gmail.com>
2022-03-08 16:53:27 +0800

    Add comment

--- ScrollingForm/ViewController.swift
+++ ScrollingForm/ViewController.swift
@@ -10,39 +10,67 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
-
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var address1TextField: UITextField!
+    @IBOutlet weak var address2TextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textFieldSetup()
         registerForKeyboardNotifications()
-        
+        // set UITapGestureRecognizer and remember to add it to the view
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(tap)
     }
     
     @objc func dismissKeyboard() {
+        // find the current text field exist, if do, force to resign from first responder
         self.view.endEditing(true)
     }
     
+    func textFieldSetup() {
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        address1TextField.delegate = self
+        address2TextField.delegate = self
+        cityTextField.delegate = self
+        stateTextField.delegate = self
+        zipCodeTextField.delegate = self
+        phoneTextField.delegate = self
+        emailTextField.delegate = self
+    }
+    // setup notificationcenter to add observer for keyboard will show/hide
     func registerForKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    
+    // use notification property keyboardFrameEndUserInfoKey to get keyboard size
     @objc func keyboardWasShown(_ notification: Notification) {
 //        guard let info = notification.userInfo,
 //              let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else { return }
-//        guard let info = notification.userInfo,
-//              let keyboardFrameValue = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-//        let keyboardFrame = keyboardFrameValue.cgRectValue
-//        let keyboardSize = keyboardFrame.size
         
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        guard let info = notification.userInfo,
+              let keyboardFrameValue = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
         
+//        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        // set contentInset to add it on scrollView and match with keyboard height
+        // contentInset is an additional blank space you can use since keyboard is overlap on you view, you create contentInset to avoid overlapping
         let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
         scrollView.contentInset = contentInsets
         scrollView.scrollIndicatorInsets = contentInsets
     }
+    // when keyboard is about to resign, set contentInset to 0 to make it back to original look
     @objc func keyboardWillBeHidden(_ notification: Notification) {
         let contentInsets = UIEdgeInsets.zero
         scrollView.contentInset = contentInsets
@@ -50,3 +78,10 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // remove the textField from first responder
+        textField.resignFirstResponder()
+        return true
+    }
+}
